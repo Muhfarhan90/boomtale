e58b11847de3fa5c0ce23edb9b0ce7dc7126ed8b
Muhammad Farhan <muhfarhanhidayatulloh@gmail.com>
2025-09-16 00:03:37 +0700

    rename route auth google

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::get('auth/google', [AuthController::class, 'redirectToGoogle'])->name('google-auth');
+Route::get('auth/google', [AuthController::class, 'redirectToGoogle'])->name('auth-google');
 Route::get('auth/google/callback', [AuthController::class, 'handleAuthGoogleCallback']);
 
 /*
